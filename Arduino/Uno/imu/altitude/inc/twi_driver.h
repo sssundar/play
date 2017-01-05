2ca9bb44f659caf77b6b55ef9ef1437f302393ad
Sushant Sundaresh <sssundar@caltech.edu>
2017-01-04 22:52:48 -0800

    TWI driver implementation started. Design changed considerably. Still need to tweak design to get same wrap-based driver SW test as the others. HW testing will have to wait till BMP180 driver tested, I think.

--- Arduino/Uno/imu/altitude/inc/twi_driver.h
+++ Arduino/Uno/imu/altitude/inc/twi_driver.h
@@ -3,8 +3,9 @@
   * @author Sushant Sundaresh
   * @date 2016-12-22
   * @details 
-  * The driver for the TWI module of the ATMEGA328p processor.
+  * The hardware abstraction layer for the TWI module of the ATMEGA328p processor.
   * Relies on register LD/ST addresses defined in io328p.h (imported in io.h)
+  * You MUST enable sleep (_ENABLE_SLEEP()) if you want to use this driver.
   */
 
 #ifndef _TWI_DRIVER_H_
@@ -28,57 +29,92 @@
 #define TWCR_TWWC   0x08    // Can probably ignore unless there are bus errors
 #define TWCR_TWEN   0x04    // Enables TWI peripheral
 #define TWCR_TWIE   0x01    // Interrupt enable
+#define TWCR_BASE   (TWCR_TWINT | TWCR_TWEN | TWCR_TWIE)
 
 /* TWSR - TWI Status Register */
 #define TWSR_PRESCALER  0x03 // Prescaler bits, set to 0 for prescale factor of 1
 #define TWSR_STATUS     0xF8 // Status bits, read only (see pages 229, 233 of Atmega 328p reference manual)
-#define MASTER_START_TRANSMITTED         0x08
-#define MASTER_ARBITRATION_LOST          0x38
-#define MASTER_TX_SLAW_ACKD              0x18
-#define MASTER_TX_SLAW_NACKD             0x20
-#define MASTER_TX_DATA_ACKD              0x28
-#define MASTER_TX_DATA_NACKD             0x30
-#define MASTER_RX_SLAR_ACKD              0x40
-#define MASTER_RX_SLAR_NACKD             0x48
-#define MASTER_RX_DATA_ACKD              0x50
-#define MASTER_RX_DATA_NACKD             0x58
+#define MASTER_START_TRANSMITTED            0x08
+#define MASTER_REPEATED_START_TRANSMITTED   0x10
+#define MASTER_ARBITRATION_LOST             0x38
+#define MASTER_TX_SLAW_ACKD                 0x18
+#define MASTER_TX_SLAW_NACKD                0x20
+#define MASTER_TX_DATA_ACKD                 0x28
+#define MASTER_TX_DATA_NACKD                0x30
+#define MASTER_RX_SLAR_ACKD                 0x40
+#define MASTER_RX_SLAR_NACKD                0x48
+#define MASTER_RX_DATA_ACKD                 0x50
+#define MASTER_RX_DATA_NACKD                0x58
 
 /* End Two Wire Interface Register Bit Masks */
 
 // @details
-// TWI driver object that really just stores one-hot FSM state variables.
-#define TWI_IS_DISABLED         0x01    
-#define TWI_IS_IDLE             0x02
-#define TWI_IS_SENDING_START    0x04    
-#define TWI_IS_SENDING_SLA_R    0x08
-#define TWI_IS_SENDING_SLA_W    0x10
-#define TWI_IS_SENDING_DATA     0x20    
-#define TWI_IS_RECEIVING_DATA   0x40 
-#define TWI_FAILED              0x80
+// TWI driver object that really just stores status codes from the last TWI action
 typedef struct sTWIDriver {    
-    uint8_t state;
+    uint8_t is_busy;
+    uint8_t status;
 } sTWIDriver;
+
+#if AVR
+// @details
+// Statically allocated TWI peripheral object
+extern sTWIDriver twi;
+#endif 
 
 // @details
 // Protects itself from interrupts but restores interrupt flags on return.
 // Sets up TWI peripheral in master transmitter/receiver mode. 
+// Sets TWBR to 0 to have a 1 MHz TWI base clock
 // Resets state variables related to the TWI tx/rx FSM. 
 // @param [in] twi a TWI driver object, uninitialized
 // @return #ksuccess: successfully initialized TWI peripheral
 // @return #kerror: if twi is null
-// @todo
-//  Set TWBR to 0 to have a 1 MHz TWI base clock
 eStatus twi_init(sTWIDriver *twi);                                         
 
-// Just disable TWI enable bit in the TWCR and clear the TWINT bit 
+// @details
+// Protects itself from interrupts but restores interrupt flags on return.
+// Just clear the TWINT bit and disable TWI enable bit in the TWCR
+// @param [in] twi a TWI driver object
 void twi_deinit(sTWIDriver *twi);                                          
 
-// Not critical. Will not return until the TWI command is complete.
-//  Set TWCR to 
-eStatus twi_get(sTWIDriver* restrict twi, uint8_t addr, uint8_t reg, uint8_t* restrict dest); 
+// @details
+// Sets up the TWI start signal and waits for the interrupt indicating this 
+// has been sent. Gets the status code.
+// @param [in] twi a TWI driver object
+// @param [in] is_repeated is this a repeated start
+// @return #ksuccess on success
+// @return #kerror otherwise
+eStatus twi_tx_start (sTWIDriver *twi, uint8_t is_repeated);
 
-// Not critical. Will not return until the TWI command is complete.
-eStatus twi_put(sTWIDriver *twi, uint8_t addr, uint8_t reg, uint8_t src);  
+// @details
+// Sets up the TWI SLA(W/R) signal and waits for the interrupt indicating this 
+// has been sent. Gets the status code.
+// @param [in] twi a TWI driver object
+// @param [in] addr TWI bus address
+// @param [in] is_write is this a write (not a read)
+// @return #ksuccess on success
+// @return #kerror otherwise
+eStatus twi_tx_sla (sTWIDriver *twi, uint8_t addr, uint8_t is_write);
+
+// @details
+// Sets up the TWDR and sends the data. Waits for the interrupt indicating this 
+// has been sent. Gets the status code, and repeats till it's out of bytes to send.
+// Then, transmits the stop condition and releases the bus.
+// @param [in] twi a TWI driver object
+// @param [in] data bytes to transmit
+// @param [in] num_bytes number of bytes to transmit
+// @return #ksuccess on success
+// @return #kerror otherwise
+eStatus twi_tx_data (sTWIDriver* restrict twi, const uint8_t* restrict data, uint8_t num_bytes);
+
+// @details
+// Receives num_bytes bytes then releases the bus.
+// @param [in] twi a TWI driver object
+// @param [in] data bytes to receive
+// @param [in] num_bytes number of bytes to receive
+// @return #ksuccess on success
+// @return #kerror otherwise
+eStatus twi_rx_data (sTWIDriver* restrict twi, uint8_t* restrict data, uint8_t num_bytes);
 
 #endif
 // _TWI_DRIVER_H_
--- Arduino/Uno/imu/altitude/twi_driver.c
+++ Arduino/Uno/imu/altitude/twi_driver.c
@@ -1,6 +1,98 @@
 #include "twi_driver.h"
 
-eStatus twi_init(sTWIDriver *twi) { (void) twi; return kfailure; }
-void twi_deinit(sTWIDriver *twi) { (void) twi; return; }
-eStatus twi_get(sTWIDriver* restrict twi, uint8_t addr, uint8_t reg, uint8_t* restrict dest) { (void) twi; (void) addr; (void) reg; (void) dest; return kerror; }
-eStatus twi_put(sTWIDriver *twi, uint8_t addr, uint8_t reg, uint8_t src) { (void) twi; (void) addr; (void) reg; (void) src; return kerror; } 
+/* Local Functions */
+
+// @details
+// Called from the TWI ISR. Gets the TWI peripheral status and sets the is_busy 
+// flag in the twi object to 0 (false).
+// @param [in] twi a TWI driver object, uninitialized
+static void twi_get_status_and_indicate_peripheral_is_no_longer_busy_ (sTWIDriver *twi) {
+    twi->status = TWSR & TWSR_STATUS;
+    twi->is_busy = 0;
+}
+
+/* Interrupt Service Routine */
+
+#if AVR
+/**
+  * @details
+  * Called after each TWI operation completes. 
+  */
+ISR(TWI_vect) {                        
+    twi_get_status_and_indicate_peripheral_is_no_longer_busy_(&twi);
+}
+#endif
+
+/* Public Functions */
+
+eStatus twi_init(sTWIDriver *twi) {
+    if (twi == NULL) { return kerror; }
+    _PROTECT(kinterrupt_save_flags);     
+    twi->is_busy = 0;
+    twi->status = 0;
+#if AVR
+    TWSR = 0; // Prescaler 1
+    TWBR = 0; // 1 MHz base clock for TWI with prescaler 1    
+#endif
+    _RELEASE(kinterrupt_restore_flags);
+    return ksuccess;
+}
+
+void twi_deinit(sTWIDriver *twi) {
+    if (twi == NULL) { return kerror; }
+    _PROTECT(kinterrupt_save_flags);     
+    twi->is_busy = 0;
+    twi->status = 0;    
+#if AVR
+    TWCR = 0;
+#endif    
+    _RELEASE(kinterrupt_restore_flags);
+}
+
+eStatus twi_tx_start (sTWIDriver *twi, uint8_t is_repeated) {
+    if (twi == NULL) { return kerror; }    
+    _PROTECT(kinterrupt_save_flags);   
+#if AVR
+    TWCR = TWCR_BASE | TWCR_TWSTA;
+#endif
+    twi->is_busy = 1;
+    _RELEASE(kinterrupt_restore_flags);
+    while (twi->is_busy) { _SLEEP(); }
+    uint8_t expected_status = is_repeated ? MASTER_REPEATED_START_TRANSMITTED : MASTER_START_TRANSMITTED;
+    return (twi->status == expected_status) ? ksuccess : kerror;
+}
+
+eStatus twi_tx_sla (sTWIDriver *twi, uint8_t addr, uint8_t is_write) {
+    if (twi == NULL) { return kerror; }    
+    _PROTECT(kinterrupt_save_flags);   
+#if AVR
+    TWDR = addr | is_write;
+    TWCR = TWCR_BASE;
+#endif
+    twi->is_busy = 1;
+    _RELEASE(kinterrupt_restore_flags);
+    while (twi->is_busy) { _SLEEP(); }
+    uint8_t expected_status = is_write ? MASTER_TX_SLAW_ACKD : MASTER_RX_SLAR_ACKD;
+    return (twi->status == expected_status) ? ksuccess : kerror;
+}
+
+eStatus twi_tx_data (sTWIDriver* restrict twi, const uint8_t* restrict data, uint8_t num_bytes) {
+    if (twi == NULL) { return kerror; }    
+    uint8_t k = 0;
+    uint8_t expected_status;
+    _PROTECT(kinterrupt_save_flags);   
+#if AVR
+    TWDR = *data;
+    I ACTUALLY NEED A HAL FOR THIS AS WELL IF I WANT TO TEST IT
+#endif
+    _RELEASE(kinterrupt_restore_flags);                    
+    for (k = 0; k < num_bytes; k++) {
+        _PROTECT(kinterrupt_save_flags);   
+        _RELEASE(kinterrupt_restore_flags);                    
+    }
+    return ksuccess;
+}
+
+eStatus twi_rx_data (sTWIDriver* restrict twi, uint8_t* restrict data, uint8_t num_bytes) {
+
+}
